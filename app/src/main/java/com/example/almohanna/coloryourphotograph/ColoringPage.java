f6f6ed311496d3324e18e596ddde8d3d09737656
AhadAlmohanna <aahad.almohanna@gmail.com>
2017-11-22 23:13:34 +0300

    نقلت الصوره من صفحة التاكد الى صفحة التلوين

--- app/src/main/java/com/example/almohanna/coloryourphotograph/ColoringPage.java
+++ app/src/main/java/com/example/almohanna/coloryourphotograph/ColoringPage.java
@@ -2,18 +2,26 @@ package com.example.almohanna.coloryourphotograph;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
-/**
- * Created by user on 27/09/17.
- */
+
 public class ColoringPage extends Activity {
+    Bitmap photo;
+    ImageView viewPhoto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.coloringpage);
+
+        photo = (Bitmap)this.getIntent().getParcelableExtra("Bitmap2");
+        setContentView(R.layout.coloringpage);
+        viewPhoto = (ImageView)findViewById(R.id.img);
+        viewPhoto.setImageBitmap(photo);
 
         // home button
         ImageButton home = (ImageButton) findViewById(R.id.home1);
@@ -26,15 +34,6 @@ public class ColoringPage extends Activity {
         });
     }
 
-    /**
-     * final View targetView = findViewById(R.id.targerView); // Any view from which you want to pick color
-     new EyeDropper(targetView, new ColorSelectionListener() {
-    @Override
-    public void onColorSelected(@ColorInt int color) {
-    // color is the color selected when you touch the targetView
-    (findViewById(R.id.colorSlot)).setBackgroundColor(color);
-    }
-    });
-     */
+
 }
 
